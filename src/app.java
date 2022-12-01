4b0730f68309676ec422043a6b7243893e051761
IsakHolm <98148805+IsakHolm@users.noreply.github.com>
2022-12-01 11:17:01 +0100

    Add files via upload

---
+++ src/app.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class app {
+
+    public static void main(String[] args) {
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File(args[2]));
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        
+        int arg0 = Integer.parseInt(args[0]);
+        int arg1 = Integer.parseInt(args[1]);
+ 
+        switch(arg0) {
+            case 1:
+                if(arg1 == 1) {
+                    Lucka1 lucka = new Lucka1(sc);
+                    System.out.println(lucka.solve1());
+                }
+                else {
+                    Lucka1 lucka = new Lucka1(sc);
+                    System.out.println(lucka.solve2());
+                }
+        }
+    }
+
+}
